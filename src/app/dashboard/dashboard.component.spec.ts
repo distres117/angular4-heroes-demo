f323c84ffba6ae61e19f9ea2d052e4358ce6c776
distres117 <omcrobbie@gmail.com>
2017-04-27 08:10:48 -0400

    chore: add more tests

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,41 +1,48 @@
+import { RouterLinkStub } from './../utils/test.helpers';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { animations } from './dashboard.animations';
 import { HttpModule } from '@angular/http';
 import { HeroServiceService } from './../services/hero-service.service';
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { DashboardComponent } from './dashboard.component';
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { NO_ERRORS_SCHEMA, DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
-describe('DashboardComponent', () => {
+fdescribe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let router: RouterLinkStub[];
 
-  beforeEach(async(() => {
+  
+
+  beforeEach(async(()=>{
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ],
-      imports:[HttpModule, BrowserAnimationsModule],
+      declarations: [ DashboardComponent],
+      imports:[HttpModule, BrowserAnimationsModule, RouterTestingModule],
       providers:[ HeroServiceService],
       schemas:[NO_ERRORS_SCHEMA]
-    })
-  }));
-
-  beforeEach(()=>{
+    });
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     let service = fixture.debugElement.injector.get(HeroServiceService);
     let spy = spyOn(service, 'getHeroes').and.returnValue(Promise.resolve([{name:'Testy', id:1}]));
-  });
+    fixture.detectChanges();
+    fixture.whenStable().then(()=>{
+      fixture.detectChanges();
+    });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('should contain 1 hero',async(()=>{
-    fixture.detectChanges();
-    fixture.whenStable().then(()=>{
-      fixture.detectChanges();
-      expect(component.heroes.length).toEqual(1);
-    });    
-  }));
+  it('should contain 1 hero',()=>{
+    expect(component.heroes.length).toEqual(1);    
+  });
+  it('should render a valid route', ()=>{
+    let button = fixture.debugElement.queryAll(By.css('a'));
+    expect(button[0].properties['href']).toEqual('/details/1');
+    
+  });
 });
